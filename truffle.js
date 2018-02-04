1be2e19ea78816092a76a4ebfabfa0560adce8f3
Gustavo Ajzenman <gustavoajz@gmail.com>
2018-02-04 15:17:09 -0300

    Add staging network for remote console.

--- truffle.js
+++ truffle.js
@@ -1,4 +1,4 @@
-const HDWalletProvider = require('truffle-hdwallet-provider')
+const HDWalletProvider = require("truffle-hdwallet-provider");
 // const mnemonic = ''
 
 module.exports = {
@@ -13,9 +13,14 @@ module.exports = {
       host: "66.165.226.146",
       port: 8545,
       // provider: new HDWalletProvider(mnemonic, 'https://ropsten.infura.io/'),
-      from: '0xC6cb7cA2470C44FDA47fac925fE59A25c0A9798D',
+      from: "0xC6cb7cA2470C44FDA47fac925fE59A25c0A9798D",
       network_id: 3, // official id of the ropsten network
       gas: 4600000
+    },
+    staging: {
+      host: "66.165.226.146",
+      port: 8545,
+      network_id: "*" // Match any network id
     }
   },
   solc: {
