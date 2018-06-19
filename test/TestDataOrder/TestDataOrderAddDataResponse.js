ff16f501acb1e04e0c8044fc6a11fe0e08194ade
Martin Manelli <manelli.ml@gmail.com>
2018-06-19 19:26:31 -0300

    DataOrder.hasSellerBeenAccepted tests

--- test/TestDataOrder/TestDataOrderAddDataResponse.js
+++ test/TestDataOrder/TestDataOrderAddDataResponse.js
@@ -2,6 +2,7 @@ const web3Utils = require('web3-utils');
 
 import { createDataOrder } from "./helpers/dataOrderCreation";
 import signMessage from "../helpers/signMessage";
+import assertRevert from "../helpers/assertRevert";
 
 contract('DataOrder', (accounts) => {
 
@@ -11,6 +12,7 @@ contract('DataOrder', (accounts) => {
   const seller = accounts[5];
   const owner = accounts[6];
   const notOwner = accounts[7];
+  const other = accounts[8];
 
   const dataHash = "9eea36c42a56b62380d05f8430f3662e7720da6d5be3bdd1b20bb16e9d";
 
@@ -44,7 +46,7 @@ contract('DataOrder', (accounts) => {
       );
       assert.fail();
     } catch (error) {
-      assert(error.toString().includes('revert'), error.toString());
+      assertRevert(error);
     }
   })
 
@@ -60,7 +62,7 @@ contract('DataOrder', (accounts) => {
       );
       assert.fail();
     } catch (error) {
-      assert(error.toString().includes('revert'), error.toString());
+      assertRevert(error);
     }
   })
 
@@ -76,7 +78,7 @@ contract('DataOrder', (accounts) => {
       );
       assert.fail();
     } catch (error) {
-      assert(error.toString().includes('revert'), error.toString());
+      assertRevert(error);
     }
   })
 
@@ -92,7 +94,7 @@ contract('DataOrder', (accounts) => {
       );
       assert.fail();
     } catch (error) {
-      assert(error.toString().includes('revert'), error.toString());
+      assertRevert(error);
     }
   })
 
@@ -108,7 +110,7 @@ contract('DataOrder', (accounts) => {
       );
       assert.fail();
     } catch (error) {
-      assert(error.toString().includes('revert'), error.toString());
+      assertRevert(error);
     }
   })
 
@@ -124,7 +126,7 @@ contract('DataOrder', (accounts) => {
       );
       assert.fail();
     } catch (error) {
-      assert(error.toString().includes('revert'), error.toString());
+      assertRevert(error);
     }
   })
 
@@ -150,7 +152,7 @@ contract('DataOrder', (accounts) => {
       );
       assert.fail();
     } catch (error) {
-      assert(error.toString().includes('revert'), error.toString());
+      assertRevert(error);
     }
   })
 
@@ -165,7 +167,7 @@ contract('DataOrder', (accounts) => {
       );
       assert.fail();
     } catch (error) {
-      assert(error.toString().includes('revert'), error.toString());
+      assertRevert(error);
     }
   })
 
@@ -180,7 +182,7 @@ contract('DataOrder', (accounts) => {
       );
       assert.fail();
     } catch (error) {
-      assert(error.toString().includes('revert'), error.toString());
+      assertRevert(error);
     }
   })
 
@@ -215,6 +217,28 @@ contract('DataOrder', (accounts) => {
     } catch (error) {
       assert.fail();
     }
+  })
+
+  it('should check if seller has been accepted', async function () {
+    await order.addDataResponse(
+      seller,
+      notary,
+      dataHash,
+      signature,
+      { from: owner }
+    );
+    const sellerWasAdded = await order.hasSellerBeenAccepted(seller);
+    assert(sellerWasAdded, "Seller was not added correctly");
+
+    try {
+      await order.hasSellerBeenAccepted('0x0');
+      assert.fail('Did not fail for 0x0 seller');
+    } catch (error) {
+      assertRevert(error);
+    }
+
+    const sellerNotExists = await order.hasSellerBeenAccepted(other);
+    assert.isNotOk(sellerNotExists, "Seller that not exists has been accepted");
   })
 
 });
