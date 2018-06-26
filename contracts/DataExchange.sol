5817efb3a5cad041115fb31b527359877aa26786
Martin Manelli <manelli.ml@gmail.com>
2018-06-26 20:55:59 -0300

    Fix failing test

--- contracts/DataExchange.sol
+++ contracts/DataExchange.sol
@@ -458,6 +458,8 @@ contract DataExchange is TokenDestructible, Pausable {
   function getNotaryInfo(
     address notary
   ) public view validAddress(notary) returns (address, string, string, string) {
+    require(allowedNotaries.exist(notary));
+
     NotaryInfo memory info = notaryInfo[notary];
     return (info.addr, info.name, info.notaryUrl, info.publicKey);
   }
