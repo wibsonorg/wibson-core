d541f6c81d3635b575fc7cfcf59540ee2378ddcd
Martin Manelli <manelli.ml@gmail.com>
2018-06-27 14:17:23 -0300

    unregisterNotary -> registerNotary

--- test/TestDataExchange/TestDataExchangeUnregisterNotary.js
+++ test/TestDataExchange/TestDataExchangeUnregisterNotary.js
@@ -26,19 +26,13 @@ contract('DataExchange', async (accounts) => {
   describe('unregisterNotary', async () => {
     it('should be called only by the owner', async () => {
       try {
-        await dataExchange.unregisterNotary(
-          notary,
-          { from: other },
-        );
+        await dataExchange.unregisterNotary(notary, { from: other });
         assert.fail();
       } catch (error) {
         assertRevert(error);
       }
 
-      const res = await dataExchange.unregisterNotary(
-        notary,
-        { from: owner },
-      );
+      const res = await dataExchange.unregisterNotary(notary, { from: owner });
       assert(res, 'Could not be called by owner');
     });
 
@@ -46,10 +40,7 @@ contract('DataExchange', async (accounts) => {
       await dataExchange.pause({ from: owner });
 
       try {
-        await dataExchange.unregisterNotary(
-          notary,
-          { from: owner },
-        );
+        await dataExchange.unregisterNotary(notary, { from: owner });
         assert.fail();
       } catch (error) {
         assertRevert(error);
@@ -57,19 +48,13 @@ contract('DataExchange', async (accounts) => {
 
       await dataExchange.unpause({ from: owner });
 
-      const res = await dataExchange.unregisterNotary(
-        notary,
-        { from: owner },
-      );
+      const res = await dataExchange.unregisterNotary(notary, { from: owner });
       assert(res, 'Could not be called when unpaused');
     });
 
     it('should fail when passed an invalid notary address', async () => {
       try {
-        await dataExchange.unregisterNotary(
-          '0x0',
-          { from: owner },
-        );
+        await dataExchange.unregisterNotary('0x0', { from: owner });
         assert.fail();
       } catch (error) {
         assertRevert(error);
@@ -77,21 +62,34 @@ contract('DataExchange', async (accounts) => {
     });
 
     it('should return false when passed an inexistent notary address', async () => {
-      const res = await dataExchange.unregisterNotary.call(
-        other,
-        { from: owner },
-      );
+      const res = await dataExchange.unregisterNotary.call(other, { from: owner });
 
       assert.isNotOk(res, 'Unregistered an inexistent notary');
     });
 
     it('should unregister a notary', async () => {
-      const res = await dataExchange.unregisterNotary(
-        notary,
-        { from: owner },
-      );
+      const res = await dataExchange.unregisterNotary(notary, { from: owner });
 
       assert(res, 'Could not unregister notary');
+    });
+
+    it('should register notary after unregistering it', async () => {
+      await dataExchange.unregisterNotary(notary, { from: owner });
+
+      const ret = await dataExchange.registerNotary(
+        notary,
+        'Notary A',
+        'Notary A URL',
+        'Notary A Public Key',
+        { from: owner },
+      );
+      assert(ret, 'Could not register notary after unregistering it');
+
+      const res = await dataExchange.getNotaryInfo(notary);
+      assert.equal(res[0], notary, 'notary address differs');
+      assert.equal(res[1], 'Notary A', 'notary name differs');
+      assert.equal(res[2], 'Notary A URL', 'notary url differs');
+      assert.equal(res[3], 'Notary A Public Key', 'notary public key differs');
     });
   });
 });
