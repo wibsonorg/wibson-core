cd3becadf318125c60bbf40b0c50a0122e0aec82
Nico <ncls.ayala@gmail.com>
2018-12-19 17:57:47 -0300

    Fix linter issues. Fix truffle version.

--- test/TestHappyPath.js
+++ test/TestHappyPath.js
@@ -2,11 +2,10 @@ import { randomIds, getPayData } from './helpers/utils';
 
 const Web3 = require('web3');
 
-const web3 = new Web3('http://localhost:8545');
 const sha3 = Web3.utils.soliditySha3;
 
 const DataExchange = artifacts.require('./DataExchange.sol');
-const DataOrder = artifacts.require('./DataOrder.sol');
+// const DataOrder = artifacts.require('./DataOrder.sol');
 const WIBToken = artifacts.require('./WIBToken.sol');
 const BatPay = artifacts.require('./BatPay.sol');
 
@@ -15,14 +14,13 @@ contract.only('DataExchange', (accounts) => {
   let batPay;
   let tx;
   let id;
-  const NOTARY_A = accounts[1];
+  // const NOTARY_A = accounts[1];
   const BUYER = accounts[4];
   const DATA_ORDER_PRICE = 20;
-  const NOTARIZATION_FEE = 10;
+  // const NOTARIZATION_FEE = 10;
   const LIST_OF_SELLERS = randomIds(1000, 50000);
   const PAYDATA = getPayData(LIST_OF_SELLERS);
   const MASTERKEY = '0x3a9c1573b2b71e6f983946fa79489682a1114193cd453bdea78717db684545b4';
-  const MASTERKEY_HASH = '0x33cfdaac3c1fd1984b81dc1c3522a76b2d2fad7dd70ca8e10ea702764eafe2a8';
   const NEW_ACCOUNT = `0x${Web3.utils.toBN(2).pow(Web3.utils.toBN(256)).subn(1).toJSON()}`;
   const BATPAY_AMOUNT = LIST_OF_SELLERS.length * DATA_ORDER_PRICE;
 
@@ -63,12 +61,16 @@ contract.only('DataExchange', (accounts) => {
 
     const PAYDATA_PADDED = PAYDATA.toArray().map(e => `0x${(`0${e.toString('16')}`).slice(-2)}`);
 
-    // tx = await batPay.transfer(id, 1, PAYDATA.toArray(), 0, payDataHash, lock, metadata, { from: BUYER });
-    tx = await batPay.transfer(id, 1, PAYDATA_PADDED, 0, payDataHash, lock, metadata, { from: BUYER });
+    // tx = await batPay
+    //   .transfer(id, 1, PAYDATA.toArray(), 0, payDataHash, lock, metadata, { from: BUYER });
+    tx = await batPay
+      .transfer(id, 1, PAYDATA_PADDED, 0, payDataHash, lock, metadata, { from: BUYER });
 
-    // // 6. Broker unlocks the payment (on notary’s behalf)
-    // // by revealing the master key used to encrypt sellers’ keys.
-    // // TODO:(through a broker?) Define broker requirements
+    assert(tx.receipt.status === '0x1');
+
+    // 6. Broker unlocks the payment (on notary’s behalf)
+    // by revealing the master key used to encrypt sellers’ keys.
+    // TODO:(through a broker?) Define broker requirements
     // tx = await batPay.unlock(0, MASTERKEY, { from: BUYER });
 
     // tx = await dataExchange.closeOrder(newOrderAddress, { from: BUYER });
