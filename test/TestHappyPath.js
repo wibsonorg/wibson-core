0d73f5f9184311e2a9e7df79c975f53179698026
Nahuel Santoalla <nahjersan@gmail.com>
2018-12-21 16:39:09 -0300

    add registerNotary to DataExchange v2

--- test/TestHappyPath.js
+++ test/TestHappyPath.js
@@ -10,7 +10,7 @@ const DataOrder = artifacts.require('./DataOrder.sol');
 const WIBToken = artifacts.require('./WIBToken.sol');
 const BatPay = artifacts.require('./BatPay.sol');
 
-contract.only('DataExchange', (accounts) => {
+contract('DataExchange', (accounts) => {
   let token;
   let batPay;
   let tx;
