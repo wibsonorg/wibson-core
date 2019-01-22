b022b2fef55c406f5c77686e9ab08a021d3a7b82
Martin Manelli <manelli.ml@gmail.com>
2019-01-22 16:06:49 -0300

    Remove unused migrations and links to unused libraries

--- migrations/3_deploy_data_exchange.js
+++ migrations/3_deploy_data_exchange.js
@@ -1,16 +1,12 @@
 const DeployUtils = require('../utils/deploymentutils');
 
-const SafeMath = artifacts.require('openzeppelin-solidity/contracts/math/SafeMath.sol');
-
 const WIBToken = artifacts.require('./WIBToken.sol');
 const DataExchange = artifacts.require('./DataExchange.sol');
 
 const deployExchange = (deployer, tokenAddress, owner) => {
   const from = owner ? { from: owner } : {};
 
-  // Can't use async-await since truffle uses thenable objects, not ES6 promises
-  return deployer.link(SafeMath, DataExchange)
-    .then(() => deployer.deploy(DataExchange, tokenAddress, from));
+  return deployer.deploy(DataExchange, tokenAddress, from);
 };
 
 module.exports = function deploy(deployer, network, accounts) {
